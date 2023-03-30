4c29031149f412b16a7241742dc487ac842c6682
Davi Ferreira da Silva <sildv45@gmail.com>
2023-03-29 22:07:18 -0300

    Outro Exemplo da Fac

---
+++ #Exemplo de entradas de dados.py
@@ -0,0 +1,26 @@
+#Exemplo de entradas de dados
+salario1 : float; salario2: float
+nome1 : str; nome2 : str
+inde : int
+sexo :str
+
+nome1 = input("Nome da primeira pessoa :")
+salrio1 = float(input("Salario da primeiroa pessoa:"))
+
+nome2 = input("Nome da segunda pessoa :")
+salario2 = float(input("Salario da segunda pessoa:"))
+
+
+
+
+idade = int(input("Digite uma idade:"))
+sexo = input("Digite um sexo (F/M):")
+
+print(f"Nome 1 :{nome1}")
+print(f"Salario 1: {salario1:.2f}")
+print(f"Nome 2 :{nome2}")
+print(f"Salario 2: {salario2:.2f}")
+print(f"idade :{idade}")
+print(f"Sexo : {sexo}")
+
+
---
+++ Model-de_class.py
@@ -0,0 +1,31 @@
+#Saida de dados com exemplos variados.
+
+print("Boa Tarde  !")
+print("A todos")
+print("Mais um aprendizado ")
+
+Idade:int
+salario:float
+altura:float
+genero:str
+nome:str
+
+idade = 20
+salario = 5800.5
+altura = 1.65
+genero = "F"
+nome = "Mariana manrial"
+
+print(f"IDADE = {idade}")
+print(f"SALARIO = {salario:.2f}")
+print(f"ALTURA = {altura:.2f}")
+print(f"GENERO = {genero}")
+print(f"nome = {nome}")
+
+print(f"A funcionaria {nome}, sexo {genero}, Seu salario e de {salario}, e tem {idade} anos")
+
+
+
+
+
+
